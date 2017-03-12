ab55564850576425d4566a97e12f5afca866f8b4
gus0302 <gustaav@gmail.com>
2017-03-11 20:21:30 -0600

    Project

---
+++ app/src/main/java/com/gustaav/mascotas/db/ConstantesBaseDatos.java
@@ -0,0 +1,17 @@
+package com.gustaav.mascotas.db;
+
+/**
+ * Created by gustavo.alvarado on 11/03/2017.
+ */
+public final class ConstantesBaseDatos {
+    public static final String BASE_DATOS_NOMBRE = "mascotas";
+    public static final int BASE_DATOS_VERSION = 1;
+
+    public static final String TABLA_MASCOTA = "mascota";
+    public static final String TABLA_MASCOTA_ID = "id";
+    public static final String TABLA_MASCOTA_NOMBRE = "nombre";
+    public static final String TABLA_MASCOTA_FOTO = "foto";
+    public static final String TABLA_MASCOTA_RATING = "rating";
+
+
+}
