04e0c7563a233dfbff7a547f64285e0dff4c45cd
gus0302 <gustaav@gmail.com>
2017-03-11 20:21:06 -0600

    Project

--- app/src/main/java/com/gustaav/mascotas/pojo/Mascota.java
+++ app/src/main/java/com/gustaav/mascotas/pojo/Mascota.java
@@ -4,16 +4,29 @@ package com.gustaav.mascotas.pojo;
  * Created by gustavo.alvarado on 09/10/2016.
  */
 public class Mascota {
+    private int id;
     private String nombre;
+    private int foto;
     private int rating;
 
     public Mascota(String nombre, int rating, int foto) {
+        this.id = id;
         this.nombre = nombre;
         this.rating = rating;
         this.foto = foto;
     }
 
-    private int foto;
+    public Mascota() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getNombre() {
         return nombre;
